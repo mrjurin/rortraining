54eabf129e93bfa1433879c95ab0860cfd90fad8
Jurin Liyun <mrjurin@gmail.com>
2018-07-08 23:30:27 +0000

    add fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit,:update, :show]
-    before_action :require_user, except: [:index,:create,:show]
+    before_action :require_user, except: [:index,:new,:show,:create]
     before_action :require_same_user, only: [:edit,:update]
     
     def new
