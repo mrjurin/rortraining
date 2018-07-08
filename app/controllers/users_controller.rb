cb6dece14f3467b3eefa07ed080739f61d5614d6
Jurin Liyun <mrjurin@gmail.com>
2018-07-08 23:18:06 +0000

    add user restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit,:update, :show]
+    before_action :require_user, except: [:index,:show]
+    before_action :require_same_user, only: [:edit,:update]
+    
     def new
         @user = User.new
     end
     
     def create
-        @user = User.new(user_params)
+       
         if @user.save
             flash[:notice] = "Welcome to the alpha blog #{@user.username}"
             redirect_to articles_path
@@ -18,7 +22,7 @@ class UsersController < ApplicationController
     end
     
     def update
-       @user= User.find(params[:id])
+       
        
        if @user.update(user_params)
            flash[:notice] = "Your account updateed successfully"
@@ -31,7 +35,6 @@ class UsersController < ApplicationController
     
     
     def show
-        @user= User.find(params[:id])
         
         @user_articles=@user.articles.paginate(page:params[:page],per_page:3)
 
@@ -45,7 +48,20 @@ class UsersController < ApplicationController
     
     
     private
-    def user_params
-        params.require(:user).permit(:username,:email,:password)
-    end
+        def user_params
+            params.require(:user).permit(:username,:email,:password)
+        end
+        
+        
+        def set_user
+            @user= User.find(params[:id])
+        end
+        
+        def require_same_user
+            if current_user !=@user
+                flash[:notice] = "you can only edit or update your own profile"
+                redirect_to root_path
+            end
+        end
+    
 end
