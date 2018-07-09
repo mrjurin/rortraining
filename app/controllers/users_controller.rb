5504c9c3329d95a362529ea5b61ec0dac2ec59e3
Jurin Liyun <mrjurin@gmail.com>
2018-07-09 14:24:54 +0000

    complete user permision

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit,:update, :show]
+    before_action :set_user, only: [:edit,:update,:show]
     before_action :require_user, except: [:index,:new,:show,:create]
-    before_action :require_same_user, only: [:edit,:update]
+    before_action :require_same_user, only: [:edit,:update,:destroy]
+    before_action :require_admin, only:[:destroy]
     
     def new
         @user = User.new
     end
     
     def create
-       
+        @user = User.new(user_params)
         if @user.save
+            session[:user_id]=@user.id
             flash[:notice] = "Welcome to the alpha blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -45,6 +47,20 @@ class UsersController < ApplicationController
     end
     
     
+    def destroy
+        @user = User.find(params[:id])
+        
+        if @user.id != current_user.id
+            @user.destroy
+            flash[:notice] = "User and all articles successfully deleted"
+            redirect_to users_path
+        else
+            flash[:notice]="Cannot delete yourself"
+            redirect_to users_path
+        end
+        
+        
+    end
     
     
     private
@@ -58,9 +74,16 @@ class UsersController < ApplicationController
         end
         
         def require_same_user
-            if current_user !=@user
+            if current_user !=@user && !current_user.admin?
                 flash[:notice] = "you can only edit or update your own profile"
                 redirect_to root_path
+            end
+        end
+        
+        def require_admin
+            if logged_in? && !current_user.admin?
+                flash[:notice] = "Only admin user can perform that actions"
+                redirect_to user_path
             end
         end
     
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625062116) do
+ActiveRecord::Schema.define(version: 20180709131943) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180625062116) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
